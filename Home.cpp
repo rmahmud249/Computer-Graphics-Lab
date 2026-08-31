bf2b9b919a434a3ffb0b033ecaac7a9a95fb9a5a
Rasel Mahmud <rmahmud.seu.bd@gmail.com>
2026-08-31 21:41:32 +0600

    pratice

--- Home.cpp
+++ Home.cpp
@@ -235,3 +235,43 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
+
+
+drawCube(ourShader, VAO, modelMatrix, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0, 0.5, 2.5, glm::vec3(0.8, 0.5, 0.2)); 
+drawCube(ourShader, VAO, modelMatrix, -0.55, 0.50, 0.0, 0.0, 0.0, 0.0, 0.2, 1.5, 1.5, glm::vec3(1.0, 0.0, 0.0)); 
+drawCube(ourShader, VAO, modelMatrix, 0.55, 0.50, 0.0, 0.0, 0.0, 0.0, 0.2, 1.5, 1.5, glm::vec3(0.0, 0.0, 1.0)); 
+drawCube(ourShader, VAO, modelMatrix, 0.0, 0.50, 0.40, 0.0, 0.0, 0.0, 2.38, 1.5, 0.2, glm::vec3(1.0, 0.7, 0.3)); 
+drawCube(ourShader, VAO, modelMatrix, 0.0, 0.50, -0.40, 0.0, 0.0, 0.0, 2.38, 1.5, 0.2, glm::vec3(0.6, 0.2, 1.0)); 
+drawCube(ourShader, VAO, modelMatrix, -0.42, 1.1, 0.0, 0.0, 0.0, 35.0, 2.2, 0.2, 1.8, glm::vec3(0.5, 0.1, 0.1)); 
+drawCube(ourShader, VAO, modelMatrix, 0.42, 1.1, 0.0, 0.0, 0.0, -35.0, 2.2, 0.2, 1.8, glm::vec3(0.5, 0.1, 0.1)); 
+drawCube(ourShader, VAO, modelMatrix, 0.0, 0.42, 0.5, 0.0, 0.0, 0.0, 0.5, 1.2, 0.15, glm::vec3(0.25, 0.12, 0.05)); 
+drawCube(ourShader, VAO, modelMatrix, -0.35, 0.50, 0.5, 0.0, 0.0, 0.0, 0.45, 0.45, 0.15, glm::vec3(0.2, 0.6, 1.0)); 
+drawCube(ourShader, VAO, modelMatrix, 0.35, 0.50, 0.5, 0.0, 0.0, 0.0, 0.45, 0.45, 0.15, glm::vec3(0.2, 0.6, 1.0)); 
+
+
+ drawCube(ourShader, VAO, modelMatrix, 2.0, 0.8, 0.0, 0.0, 0.0, 0.0, 1.2, 1.5, 0.8, glm::vec3(0.3, 0.3, 0.3));
+ drawCube(ourShader, VAO, modelMatrix, 2.1, 1.6, 0.0, 0.0, 0.0, 0.0, 1.0, 0.8, 0.8, glm::vec3(0.5, 0.5, 0.5));
+ drawCube(ourShader, VAO, modelMatrix, 1.5, 0.9, 0.0, 0.0, 0.0, -45.0, 0.3, 1.2, 0.4, glm::vec3(0.2, 0.4, 0.8));
+ drawCube(ourShader, VAO, modelMatrix, 2.7, 0.9, 0.0, 0.0, 0.0, 45.0, 0.3, 1.2, 0.4, glm::vec3(0.2, 0.4, 0.8));
+ drawCube(ourShader, VAO, modelMatrix, 1.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 1.0, 0.5, glm::vec3(0.2, 0.2, 0.2));
+ drawCube(ourShader, VAO, modelMatrix, 2.4, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 1.0, 0.5, glm::vec3(0.2, 0.2, 0.2));
+ drawCube(ourShader, VAO, modelMatrix, 2.2, 1.9, 0.4, 0.0, 0.0, 0.0, 0.15, 0.15, 0.1, glm::vec3(0.0, 1.0, 1.0));
+ drawCube(ourShader, VAO, modelMatrix, 2.6, 1.9, 0.4, 0.0, 0.0, 0.0, 0.15, 0.15, 0.1, glm::vec3(0.0, 1.0, 1.0));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
